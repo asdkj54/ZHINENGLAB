752aca5bcf3db1f9b3e0ff2c67b67233d7800ad5
asdkj54 <1450438214@qq.com>
2019-06-11 21:38:34 +0800

    更改

--- MFCApplication2/resource.h
+++ MFCApplication2/resource.h
@@ -13,6 +13,7 @@
 #define IDC_COMBO1                      1002
 #define IDC_BUTTON3                     1005
 #define IDC_COMBO2                      1006
+#define IDC_BUTTON2                     1008
 
 // Next default values for new objects
 // 
@@ -20,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        139
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
