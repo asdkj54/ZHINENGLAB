766966f8d6dc5e5b7a4d4c97face90e5dda98821
asdkj54 <1450438214@qq.com>
2019-07-23 18:55:47 +0800

    0723_0_1

--- LABmedic/CmediclogQuery.cpp
+++ LABmedic/CmediclogQuery.cpp
@@ -72,59 +72,7 @@ BOOL CmediclogQuery::OnInitDialog()
 	//LVS_EX_GRIDLINES是希望显示网格；LVS_EX_FULLROWSELECT是希望被选中时整行反色显示；LVS_EX_HEADERDRAGDROP是让其支持点击表头排序；LVS_EX_TWOCLICKACTIVATE是希望有鼠标在未被选中的行上移动的时候有一些效果
 	m_listQuery.SetExtendedStyle(m_listQuery.GetExtendedStyle() | LVS_EX_GRIDLINES | LVS_EX_FULLROWSELECT | LVS_EX_HEADERDRAGDROP/* | LVS_EX_TWOCLICKACTIVATE*/);
 	
-	/*CString str[] = { TEXT("序号"),TEXT("日期"),TEXT("人员"),TEXT("药品"),TEXT("领用时间"),TEXT("药品用途/用量") };
-	for (int i = 0; i < 6; i++)
-	{
-		m_listQuery.InsertColumn(i, str[i], LVCFMT_LEFT, 200);
-
-	}
-
-	m_listQuery.InsertItem(0, TEXT("1"));
-	m_listQuery.SetItemText(0, 1,TEXT("2019年5月1号"));
-	m_listQuery.SetItemText(0, 2, TEXT("人员2"));
-	m_listQuery.SetItemText(0, 3, TEXT("石油醚"));
-	m_listQuery.SetItemText(0, 4, TEXT("2019年5月1号11时21分33秒"));
-	m_listQuery.SetItemText(0, 5, TEXT("实验/30(单位)"));
-
-
-	m_listQuery.InsertItem(1, TEXT("2"));
-	m_listQuery.SetItemText(1, 1,TEXT("2019年5月2号"));
-	m_listQuery.SetItemText(1, 2, TEXT("人员3"));
-	m_listQuery.SetItemText(1, 3, TEXT("四氯化碳"));
-	m_listQuery.SetItemText(1, 4, TEXT("2019年5月2号14时29分20秒"));
-	m_listQuery.SetItemText(1, 5, TEXT("实验/100(单位)"));
-
-	m_listQuery.InsertItem(2, TEXT("3"));
-	m_listQuery.SetItemText(2, 1, TEXT("2019年5月7号"));
-	m_listQuery.SetItemText(2, 2, TEXT("人员3"));
-	m_listQuery.SetItemText(2, 3, TEXT("氯化钡"));
-	m_listQuery.SetItemText(2, 4, TEXT("2019年5月7号15时10分07秒"));
-	m_listQuery.SetItemText(2, 5, TEXT("实验/80(单位)"));
-
-	m_listQuery.InsertItem(3, TEXT("4"));
-	m_listQuery.SetItemText(3, 1, TEXT("2019年5月9号"));
-	m_listQuery.SetItemText(3, 2, TEXT("人员3"));
-	m_listQuery.SetItemText(3, 3, TEXT("甲醇"));
-	m_listQuery.SetItemText(3, 4, TEXT("2019年5月9号10时31分53秒"));
-	m_listQuery.SetItemText(3, 5, TEXT("实验/80(单位)"));
-
-	m_listQuery.InsertItem(4, TEXT("5"));
-	m_listQuery.SetItemText(4, 1, TEXT("2019年5月14号"));
-	m_listQuery.SetItemText(4, 2, TEXT("人员3"));
-	m_listQuery.SetItemText(4, 3, TEXT("氯化钡"));
-	m_listQuery.SetItemText(4, 4, TEXT("2019年5月14号15时25分03秒"));
-	m_listQuery.SetItemText(4, 5, TEXT("实验/80(单位)"));
-
-
-	m_listQuery.InsertItem(5, TEXT("6"));
-	m_listQuery.SetItemText(5, 1, TEXT("2019年5月17号"));
-	m_listQuery.SetItemText(5, 2, TEXT("人员3"));
-	m_listQuery.SetItemText(5, 3, TEXT("乙醇"));
-	m_listQuery.SetItemText(5, 4, TEXT("2019年5月17号10时12分21秒"));
-	m_listQuery.SetItemText(5, 5, TEXT("实验/80(单位)"));
-
-	m_listQuery.SetExtendedStyle(m_listQuery.GetExtendedStyle() | LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
-*/
+	
 	return TRUE;  // return TRUE unless you set the focus to a control
 				  // 异常: OCX 属性页应返回 FALSE
 }
@@ -143,14 +91,7 @@ void CmediclogQuery::OnBnClickedButton2()
 	vName.push_back("领用时间");
 	vName.push_back("药品用途");
 	vName.push_back("药品用量");
-	//查询结果
-	//UpdateData(TRUE);                       //这个函数的使用请参考数据交换UpdateData，那篇文章  
-	//int index = m_comboxm1.GetCurSel();       //这个函数用于得到用户选择的是下拉列表中的第几行，第一行的话，返回0，依次类推  
-	//CString strI;
-	//strI.Format(_T("%d"), index);
-	//CString strC;
-	//m_comboxm1.GetLBText(index, strC);        //根据行号，这个可以得到那行的字符串  
-	//CString strSQL; //存放sql语句
+
 	vector<vector<_variant_t>> vResult(m_DataBase.Select(::SysAllocString(L"select * from LABmedic order by 领用时间 desc"), vName));
 	
 	m_listQuery.DeleteAllItems();	//删除所有的项目
@@ -323,6 +264,7 @@ CString CmediclogQuery::VariantToCString(_variant_t var)
 		str.Format(L"%d", var.lVal);
 		break;
 
+
 	case VT_R4:         //var is float type
 		str.Format(L"%10.6f", (double)var.fltVal);
 		break;
@@ -382,7 +324,7 @@ void CmediclogQuery::OnBnClickedButton5()
 	CString strC2;
 	m_medcombox2.GetLBText(index2, strC2);
 
-	int index3 = m_medcombox3.GetCurSel();       //这个函数用于得到用户选择的是下拉列表中的第几行，第一行的话，返回0，依次类推  
+	int index3 = m_medcombox3.GetCurSel();     
 	CString strI3;
 	strI3.Format(_T("%d"), index3);
 	CString strC3;
